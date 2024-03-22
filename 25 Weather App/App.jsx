a174e00c8355d78a51e37e35e7ffce3e341cdb4b
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-03-22 03:04:55 -0400

    Complete challenge 25

--- 25 Weather App/App.jsx
+++ 25 Weather App/App.jsx
@@ -1,4 +1,5 @@
-import React from "react"
+import React, { useState } from "react"
+import { weatherData } from "./data.ts"
 
 /* Challenge
 
@@ -21,49 +22,47 @@ The weather data is currently hardcoded into the JSX. Your task is to insert it 
 */
 
 export default function App() {
-  
-  const weatherData = [
-    {
-      id: 0,
-      day: "Monday",
-      condition: "Sunny",
-      lowTemp: 20,
-      highTemp: 38
-    },
-    {
-      id: 1,
-      day: "Tuesday",
-      condition: "Rainy",
-      lowTemp: 8,
-      highTemp: 15
-    },
-    {
-      id: 2,
-      day: "Wednesday",
-      condition: "Snowy",
-      lowTemp: -5,
-      highTemp: 3
-    }
-  ]
-  
+  const [currentArrayIndex, setCurrentArrayIndex] = useState(0)
+
+  const handleClick = () => {
+    setCurrentArrayIndex((prevIndex) => {
+      return prevIndex < weatherData.length - 1 ? prevIndex + 1 : 0
+    })
+  }
+
   return (
-    <div className="app-container sunny-background"> 
+    <div
+      className={`app-container ${weatherData[
+        currentArrayIndex
+      ].condition.toLowerCase()}-background`}>
       <div className="weather-container">
-        <div className="icon">☀️</div>
-        <div className="condition-text">Sunny</div>
-        <div className="temp-range-container">
-            <div className="low-temp-container">
-              <p className="low-temp-data">20°</p>
-              <p>Low</p>
-            </div>
-            <div className="high-temp-container">
-              <p className="high-temp-data">38°</p>
-              <p>High</p>
-            </div>
+        <div className="icon">
+          {weatherData[currentArrayIndex].condition === "Sunny"
+            ? "☀️"
+            : weatherData[currentArrayIndex].condition === "Rainy"
+            ? "🌧️"
+            : "❄️"}
         </div>
-        <div className="day">Monday</div>
+        <div className="condition-text">
+          {weatherData[currentArrayIndex].condition}
+        </div>
+        <div className="temp-range-container">
+          <div className="low-temp-container">
+            <p className="low-temp-data">
+              {weatherData[currentArrayIndex].lowTemp}
+            </p>
+            <p>Low</p>
+          </div>
+          <div className="high-temp-container">
+            <p className="high-temp-data">
+              {weatherData[currentArrayIndex].highTemp}
+            </p>
+            <p>High</p>
+          </div>
+        </div>
+        <div className="day">{weatherData[currentArrayIndex].day}</div>
       </div>
-      <button>Test</button>
+      <button onClick={handleClick}>Test</button>
     </div>
   )
 }
